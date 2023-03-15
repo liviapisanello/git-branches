668bd91c432adbcbdf2a5a06f3132185571cb4ca
Lívia Laur Pisanello <livia.pisanello@germinare.org.br>
2023-03-15 19:21:36 -0300

    Concertando os erros do Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,9 +1,6 @@
 public class Cliente {
-<<<<<<< HEAD
     private String livia;
-=======
     private String Rapha;
->>>>>>> origin/classe-cliente
     private String cpf;
     private int agencia;
     private double saldo;
